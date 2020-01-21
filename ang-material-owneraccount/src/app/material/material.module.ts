66ff7ce43495300b657eecab84c4dfc2d414596d
Janco <jsteenkamp@breitech.co.za>
2020-01-21 16:06:19 +0200

    Created Top Nave And Side nav
    Next: Material table

---
+++ ang-material-owneraccount/src/app/material/material.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTabsModule,
+         MatSidenavModule,
+         MatToolbarModule,
+         MatButtonModule,
+         MatIconModule,
+         MatListModule,  
+         MatMenuModule} from '@angular/material';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule
+  ],
+  exports:[
+    MatTabsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule
+  ]
+})
+export class MaterialModule { }
